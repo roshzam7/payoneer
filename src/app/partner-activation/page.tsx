4f64fd93e39d5c003b3d231ae4bbbcbc5084ff9b
roshzam7 <zzam3571@gmail.com>
2025-08-25 10:08:03 +0000

    age added

--- src/app/partner-activation/page.tsx
+++ src/app/partner-activation/page.tsx
@@ -10,6 +10,7 @@ import Sectionthree2 from "../assets/images/Sectionthree/Sectionthree2.png";
 import Sectionthree3 from "../assets/images/Sectionthree/Sectionthree3.png";
 import Sectionthree4 from "../assets/images/Sectionthree/Sectionthree4.png";
 import Footer from "../components/Footer";
+import Newpage from "../newpage/page";
 
 
 const topics = [
@@ -387,6 +388,8 @@ export default function ResellerLandingPage() {
               Coming Soon..
             </h2>
           </section>
+          <Newpage />
+
         </main>
       </div>
     <Footer />
